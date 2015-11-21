acc4e70f16f05bd90339af9be94b806a5798d96d
Dawie van Lill <djvanlill@gmail.com>
2015-11-22 01:06:34 +0200

    Awesome, Pycharm has Git integration.

--- L3B.py
+++ L3B.py
@@ -9,3 +9,5 @@ while number > 0:
 
 
 #Change something here so that I can commit a change. 
+
+#Ok, let us try and work with this IDE. I think I like Atom more. But this isn't a bad environment either.
