d0d58a36c2b6e9456c1d3a5b024acbb3e61df3be
Dawie van Lill <djvanlill@gmail.com>
2015-11-22 01:26:17 +0200

    Use both atom and Pycharm... why not?

--- L3B.py
+++ L3B.py
@@ -8,6 +8,8 @@ while number > 0:
 	number -= 2
 
 
-#Change something here so that I can commit a change. 
+#Change something here so that I can commit a change.
 
 #Ok, let us try and work with this IDE. I think I like Atom more. But this isn't a bad environment either.
+
+# I like the way this looks better than PyCharm. Will perhaps use both. But I really like atom. 
