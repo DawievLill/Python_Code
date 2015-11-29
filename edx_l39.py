c3035dc88a982466e51284245c74f869de51d095
Dawie van Lill <djvanlill@gmail.com>
2015-11-29 16:06:44 +0200

    Update to most recent version

--- edx_l39.py
+++ edx_l39.py
@@ -22,22 +22,34 @@
 
 # We know that the x input must come from raw input by the user.
 
-x = raw_input("Please think of a number between 0 and 100!")
-# epsilon = 0.01
-# numGuesses = 0
-low = 0.0
+print("Please think of a number between 0 and 100!")
+x = int(raw_input())
+low = 0
 high = x
-ans = (high + low ) /2.0
+ans = (high + low)/2
 
-print "Is your secret number %r" % (ans)
+print "Is your secret number", ans
 
-print \
-("Enter 'h' to indicate that the guess is too high. Enter 'l' to indicate that the guess is too low. Enter 'c' to indicate I guessed correctly.")
+print ("Enter 'h' to indicate that the guess is too high. Enter 'l' to indicate that the guess is too low. Enter 'c' to indicate I guessed correctly.")
 
-y = raw_input()
+#Up until this point it works fine
 
+x = raw_input()
 
+if x == 'h':
+    low = 0
+    high = ans
+    ansh = (high + low)/2
+    print "Is your secret number", ansh
 
+elif x == 'l':
+    low = ans
+    high = 100
+    ansl = (high + low)/2
+    print "Is your secret number", ansl
 
+elif x == 'c':
+    print ("Cool")
 
-
+else:
+    print "I don't know what this means"
