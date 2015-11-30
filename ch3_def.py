67376a6bc15daf023663b73997aa9099bb0c446e
Dawie van Lill <djvanlill@gmail.com>
2015-11-30 23:39:52 +0200

    First notes on Ch3 of How to Think Like a CS book

---
+++ ch3_def.py
@@ -0,0 +1,25 @@
+
+#A function is a named sequence of statements that performs a desired operation
+
+#Can make up any name for the functions that you create
+#Can be any nmber of statements inside a function
+
+#An example of what a function looks like, without parameters
+
+def new_line():
+    print
+
+#Function is named new_line
+#Defining a new function does not make it run!
+## We need a FUNCTION CALL...
+## Contains the same name as the function
+
+print "First line"
+new_line()
+print "Second line"
+
+#You get the idea, in fact I have done this before.
+#Function has to be created before it can be called.
+
+##Flow of execution
+#Statements inside the function are not executed until the function is called
