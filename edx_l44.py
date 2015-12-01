c27ab575889da3ab7094b858a8e91575f246f405
Dawie van Lill <djvanlill@gmail.com>
2015-12-01 16:18:29 +0200

    Quadratic function

---
+++ edx_l44.py
@@ -0,0 +1,15 @@
+
+
+#Need to provide a definition that returns the value of a quadratic equation
+#Takes four values and returns a single number
+
+#Define the values of the quadratic
+# a = 1
+# b = 2
+# c = 10
+# x = 15
+
+def evalQuadratic(a, b, c, x):
+    return a*(x*x) + b*x + c
+
+print evalQuadratic(1,2,10,15)
